2ae382b1b255146c303d0463140f2a3596d58594
은승균 <dmstmdrbs98@gmail.com>
2022-07-02 23:50:14 +0900

    feat: add item component for study list
    스터디 리스트 아이템 컴포넌트 추가
      - UI 구성
      - 메인 페이지 더미 렌더링

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,6 +7,7 @@ import { PageContainer } from '@/CommonStyles';
 import FloatingButton from '@/components/FloatingButton';
 import FloatingOpenMenuButton from '@/components/FloatingButton/FloatingOpenMenuButton';
 import Layout from '@/components/Layout';
+import StudyListItem from '@/components/StudyListItem';
 
 const Contents = styled.div`
   overflow: scroll;
@@ -42,7 +43,11 @@ const Main = () => {
   return (
     <Layout title='스터디 목록'>
       <PageContainer onClick={closeFloatingMenu}>
-        <Contents />
+        <Contents>
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((studyItem) => (
+            <StudyListItem key={studyItem} />
+          ))}
+        </Contents>
         <FloatingOpenMenuButton
           content='M'
           primary
