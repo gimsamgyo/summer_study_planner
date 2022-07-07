7b6b57033392153acc316c0e141a9a3b64c60f9d
coreight98 <maxcha98@naver.com>
2022-07-08 02:13:56 +0900

    refactor: divide route page

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-import Navigate from './components/Navigate';
+import { store } from './app/store';
 import GlobalStyles from './GlobalStyles';
-import Calender from './pages/Calender';
-import Create from './pages/Create';
-import Main from './pages/Main';
-import Penalty from './pages/Penalty';
-import User from './pages/User';
+import { Pages } from './Pages';
 
 const App = () => (
-  <>
+  <Provider store={store}>
     <GlobalStyles />
     <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={<Navigate />}
-        >
-          <Route
-            index
-            element={<Main />}
-          />
-          <Route
-            path='/create'
-            element={<Create />}
-          />
-          <Route
-            path='/penalty'
-            element={<Penalty />}
-          />
-          <Route
-            path='/calender'
-            element={<Calender />}
-          />
-          <Route
-            path='/user'
-            element={<User />}
-          />
-        </Route>
-      </Routes>
+      <Pages />
     </BrowserRouter>
-  </>
+  </Provider>
 );
 
 export default App;
