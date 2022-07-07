772e40b719bb1fe83f89f1b6e66dcf7a8578efb6
coreight98 <maxcha98@naver.com>
2022-07-08 02:17:36 +0900

    feat: add calender detail page

---
+++ src/pages/CalenderDetail.tsx
@@ -0,0 +1,13 @@
+// import styled from 'styled-components';
+
+import Layout from '@/components/Layout';
+
+const CalenderDetail = () => (
+  <Layout
+    title='캘린더'
+    canGoBack
+  >
+    <div />
+  </Layout>
+);
+export default CalenderDetail;
